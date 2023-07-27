d28c9ec63e582b07ec6f843356d8788c98aa708a
KorneliyaB <korneliyabanyas@gmail.com>
2023-07-27 12:34:09 -0500

    DB

---
+++ src/test/java/steps/AddJobSteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+
+public class AddJobSteps {
+    @Then("user clicks on the admin button")
+    public void user_clicks_on_the_admin_button() {
+
+    }
+    @Then("user click on the job")
+    public void user_click_on_the_job() {
+
+    }
+    @Then("user Click on Job Title")
+    public void user_click_on_job_title() {
+
+    }
+    @Then("user clicks on the add button")
+    public void user_clicks_on_the_add_button() {
+
+    }
+    @Then("user enters job {string} title")
+    public void user_enters_job_title(String string) {
+
+    }
+    @Then("user enters job description {string}")
+    public void user_enters_job_description(String string) {
+
+    }
+    @Then("user clicks on the save button")
+    public void user_clicks_on_the_save_button() {
+
+    }
+    @Then("verify data is stored properly in database")
+    public void verify_data_is_stored_properly_in_database() {
+
+    }
+
+}
